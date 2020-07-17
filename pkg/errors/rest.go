f4a0219d73828d00d432c416e6b432191e5a76c0
Alex Ripoll <alexripoll@MAC-1611-098.local>
2020-07-17 12:34:59 +0200

    Add http rest error management

---
+++ pkg/errors/rest.go
@@ -0,0 +1,44 @@
+package errors
+
+import (
+	"net/http"
+)
+
+type Rest struct {
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+	Error   string `json:"error"`
+}
+
+func NewBadRequestError(message string) *Rest {
+	return &Rest{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Error:   "bad_request",
+	}
+}
+
+func NewNotFoundError(message string) *Rest {
+	return &Rest{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "not_found",
+	}
+}
+
+func NewInternalServerError(message string) *Rest {
+	return &Rest{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Error:   "internal_server_error",
+	}
+}
+
+func NewError(status int, message string) *Rest {
+	return &Rest{
+		Message: message,
+		Status:  status,
+		Error:   "error",
+	}
+}
+
